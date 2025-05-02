9ec3f7fb852870d235257fec217ccf7c6fc0523b
Vlad Serebrennikov <serebrennikov.vladislav@gmail.com>
2025-05-02 09:36:59 +0300

    [clang][NFC] Reland "Convert `Sema::TryCaptureKind` to scoped enum"

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -648,6 +648,8 @@ enum class TrivialABIHandling {
   ConsiderTrivialABI
 };
 
+enum class TryCaptureKind { Implicit, ExplicitByVal, ExplicitByRef };
+
 /// Sema - This implements semantic analysis and AST building for C.
 /// \nosubgrouping
 class Sema final : public SemaBase {
@@ -6793,12 +6795,6 @@ public:
   ExprResult CheckLValueToRValueConversionOperand(Expr *E);
   void CleanupVarDeclMarking();
 
-  enum TryCaptureKind {
-    TryCapture_Implicit,
-    TryCapture_ExplicitByVal,
-    TryCapture_ExplicitByRef
-  };
-
   /// Try to capture the given variable.
   ///
   /// \param Var The variable to capture.
@@ -6840,7 +6836,7 @@ public:
 
   /// Try to capture the given variable.
   bool tryCaptureVariable(ValueDecl *Var, SourceLocation Loc,
-                          TryCaptureKind Kind = TryCapture_Implicit,
+                          TryCaptureKind Kind = TryCaptureKind::Implicit,
                           SourceLocation EllipsisLoc = SourceLocation());
 
   /// Checks if the variable must be captured.
