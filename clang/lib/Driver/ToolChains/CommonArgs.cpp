ffd00fbf77a40347ba2a9b7600ffff5f8ec9867e
Kazu Hirata <kazu@google.com>
2025-05-04 10:51:40 -0700

    [clang] Remove unused local variables (NFC) (#138453)

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -2625,7 +2625,6 @@ static void GetSDLFromOffloadArchive(
 
   C.addTempFile(C.getArgs().MakeArgString(OutputLib));
 
-  ArgStringList CmdArgs;
   SmallString<128> DeviceTriple;
   DeviceTriple += Action::GetOffloadKindName(JA.getOffloadingDeviceKind());
   DeviceTriple += '-';
