27cf837fb50adc8df394f7e17458961acfe97d6d
Fangrui Song <i@maskray.me>
2025-05-20 08:54:53 -0700

    [llvm-objdump] --adjust-vma: Call getInstruction with adjusted address
    llvm-objdump currently calls MCDisassembler::getInstruction with
    unadjusted address and MCInstPrinter::printInst with adjusted address.
    The decoded branch targets will be adjusted as expected for most targets
    (as the getInstruction address is insignificant) but not for SystemZ
    (where the getInstruction address is displayed).
    
    Specify an adjust address to fix SystemZInstPrinter output.
    
    The added test utilizes llvm/utils/update_test_body.py to make updates
    easier and additionally checks that we don't adjust SHN_ABS symbol
    addresses.
    
    Pull Request: https://github.com/llvm/llvm-project/pull/140471

--- llvm/tools/llvm-objdump/llvm-objdump.cpp
+++ llvm/tools/llvm-objdump/llvm-objdump.cpp
@@ -2329,7 +2329,7 @@ disassembleObject(ObjectFile &Obj, const ObjectFile &DbgObj,
           // provided
           MCInst Inst;
           ArrayRef<uint8_t> ThisBytes = Bytes.slice(Index);
-          uint64_t ThisAddr = SectionAddr + Index;
+          uint64_t ThisAddr = SectionAddr + Index + VMAAdjustment;
           bool Disassembled = DT->DisAsm->getInstruction(
               Inst, Size, ThisBytes, ThisAddr, CommentStream);
           if (Size == 0)
