062fa97cb055732d25cd08d6961daa6c456c2762
Pierre van Houtryve <pierre.vanhoutryve@amd.com>
2025-05-22 11:42:01 +0200

    [CodeGen] Add SSID & Atomic Ordering to IntrinsicInfo (#140896)
    getTgtMemIntrinsic should be able to propagate such information to the
    MMO

--- llvm/include/llvm/CodeGen/TargetLowering.h
+++ llvm/include/llvm/CodeGen/TargetLowering.h
@@ -1229,6 +1229,9 @@ public:
     MaybeAlign align = Align(1);   // alignment
 
     MachineMemOperand::Flags flags = MachineMemOperand::MONone;
+    SyncScope::ID ssid = SyncScope::System;
+    AtomicOrdering order = AtomicOrdering::NotAtomic;
+    AtomicOrdering failureOrder = AtomicOrdering::NotAtomic;
     IntrinsicInfo() = default;
   };
 
