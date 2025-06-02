1a1ae3a03818f59d95645ce91fc8c0c4c487def2
Maksim Panchenko <maks@fb.com>
2025-06-02 12:33:54 -0700

    [BOLT] Fix references in ignored functions in CFG state (#140678)
    When we call setIgnored() on functions that already have CFG built,
    these functions are not going to get emitted and we risk missing
    external function references being updated.
    
    To mitigate the potential issues, run scanExternalRefs() on such
    functions to create patches/relocations.
    
    Since scanExternalRefs() relies on function relocations, we have to
    preserve relocations until the function is emitted. As a result, the
    memory overhead without debug info update could reach up to 2%.

--- bolt/lib/Rewrite/RewriteInstance.cpp
+++ bolt/lib/Rewrite/RewriteInstance.cpp
@@ -3420,6 +3420,7 @@ void RewriteInstance::disassembleFunctions() {
         BC->outs() << "BOLT-INFO: could not disassemble function " << Function
                    << ". Will ignore.\n";
       // Forcefully ignore the function.
+      Function.scanExternalRefs();
       Function.setIgnored();
     });
 
