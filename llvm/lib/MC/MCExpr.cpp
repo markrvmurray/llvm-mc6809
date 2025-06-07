5648981fd5ca7897c64675f093c57541cfede309
Fangrui Song <i@maskray.me>
2025-06-07 11:33:40 -0700

    MC: Add MCSpecifierExpr to unify target MCExprs
    Many targets define MCTargetExpr subclasses just to encode an expression
    with a relocation specifier. Create a generic MCSpecifierExpr to be
    inherited instead. Migrate M68k and SPARC as examples.

--- llvm/lib/MC/MCExpr.cpp
+++ llvm/lib/MC/MCExpr.cpp
@@ -16,6 +16,7 @@
 #include "llvm/MC/MCAssembler.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCObjectWriter.h"
+#include "llvm/MC/MCStreamer.h"
 #include "llvm/MC/MCSymbol.h"
 #include "llvm/MC/MCValue.h"
 #include "llvm/Support/Casting.h"
@@ -172,6 +173,9 @@ void MCExpr::print(raw_ostream &OS, const MCAsmInfo *MAI,
       OS << ')';
     return;
   }
+
+  case MCExpr::Specifier:
+    return cast<MCSpecifierExpr>(this)->printImpl(OS, MAI);
   }
 
   llvm_unreachable("Invalid expression kind!");
@@ -208,6 +212,9 @@ bool MCExpr::isSymbolUsedInExpression(const MCSymbol *Sym) const {
         static_cast<const MCUnaryExpr *>(this)->getSubExpr();
     return SubExpr->isSymbolUsedInExpression(Sym);
   }
+  case MCExpr::Specifier:
+    return static_cast<const MCSpecifierExpr *>(this)->isSymbolUsedInExpression(
+        Sym);
   }
 
   llvm_unreachable("Unknown expr kind!");
@@ -703,6 +710,8 @@ bool MCExpr::evaluateAsRelocatableImpl(MCValue &Res, const MCAssembler *Asm,
 
     return true;
   }
+  case Specifier:
+    return cast<MCSpecifierExpr>(this)->evaluateAsRelocatableImpl(Res, Asm);
   }
 
   llvm_unreachable("Invalid assembly expression kind!");
@@ -751,7 +760,21 @@ MCFragment *MCExpr::findAssociatedFragment() const {
     // Otherwise, return the first non-null fragment.
     return LHS_F ? LHS_F : RHS_F;
   }
+
+  case Specifier:
+    return cast<MCSpecifierExpr>(this)->getSubExpr()->findAssociatedFragment();
   }
 
   llvm_unreachable("Invalid assembly expression kind!");
+}
+
+MCSpecifierExpr::~MCSpecifierExpr() {}
+
+bool MCSpecifierExpr::evaluateAsRelocatableImpl(MCValue &Res,
+                                                const MCAssembler *Asm) const {
+  if (!getSubExpr()->evaluateAsRelocatable(Res, Asm))
+    return false;
+
+  Res.setSpecifier(specifier);
+  return !Res.getSubSym();
 }
