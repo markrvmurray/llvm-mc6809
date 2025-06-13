abd585852995843e576b623687a7acec1ad940ff
Aaron Ballman <aaron@aaronballman.com>
2025-06-13 06:45:40 -0400

    Remove delayed typo expressions (#143423)
    This removes the delayed typo correction functionality from Clang
    (regular typo correction still remains) due to fragility of the
    solution.
    
    An RFC was posted here:
    https://discourse.llvm.org/t/rfc-removing-support-for-delayed-typo-correction/86631
    and while that RFC was asking for folks to consider stepping up to be
    maintainers, and we did have a few new contributors show some interest,
    experiments show that it's likely worth it to remove this functionality
    entirely and focus efforts on improving regular typo correction.
    
    This removal fixes ~20 open issues (quite possibly more), improves
    compile time performance by roughly .3-.4%
    (https://llvm-compile-time-tracker.com/?config=Overview&stat=instructions%3Au&remote=AaronBallman&sortBy=date),
    and does not appear to regress diagnostic behavior in a way we wouldn't
    find acceptable.
    
    Fixes #142457
    Fixes #139913
    Fixes #138850
    Fixes #137867
    Fixes #137860
    Fixes #107840
    Fixes #93308
    Fixes #69470
    Fixes #59391
    Fixes #58172
    Fixes #46215
    Fixes #45915
    Fixes #45891
    Fixes #44490
    Fixes #36703
    Fixes #32903
    Fixes #23312
    Fixes #69874

--- clang/lib/Parse/ParseStmt.cpp
+++ clang/lib/Parse/ParseStmt.cpp
@@ -601,7 +601,7 @@ StmtResult Parser::ParseSEHExceptBlock(SourceLocation ExceptLoc) {
   {
     ParseScopeFlags FilterScope(this, getCurScope()->getFlags() |
                                           Scope::SEHFilterScope);
-    FilterExpr = Actions.CorrectDelayedTyposInExpr(ParseExpression());
+    FilterExpr = ParseExpression();
   }
 
   if (getLangOpts().Borland) {
@@ -1831,11 +1831,7 @@ StmtResult Parser::ParseDoStatement() {
 
   SourceLocation Start = Tok.getLocation();
   ExprResult Cond = ParseExpression();
-  // Correct the typos in condition before closing the scope.
-  if (Cond.isUsable())
-    Cond = Actions.CorrectDelayedTyposInExpr(Cond, /*InitDecl=*/nullptr,
-                                             /*RecoverUncorrectedTypos=*/true);
-  else {
+  if (!Cond.isUsable()) {
     if (!Tok.isOneOf(tok::r_paren, tok::r_square, tok::r_brace))
       SkipUntil(tok::semi);
     Cond = Actions.CreateRecoveryExpr(
@@ -2017,7 +2013,7 @@ StmtResult Parser::ParseForStatement(SourceLocation *TrailingElseLoc) {
     }
   } else {
     ProhibitAttributes(attrs);
-    Value = Actions.CorrectDelayedTyposInExpr(ParseExpression());
+    Value = ParseExpression();
 
     ForEach = isTokIdentifier_in();
 
@@ -2176,12 +2172,10 @@ StmtResult Parser::ParseForStatement(SourceLocation *TrailingElseLoc) {
   StmtResult ForEachStmt;
 
   if (ForRangeInfo.ParsedForRangeDecl()) {
-    ExprResult CorrectedRange =
-        Actions.CorrectDelayedTyposInExpr(ForRangeInfo.RangeExpr.get());
     ForRangeStmt = Actions.ActOnCXXForRangeStmt(
         getCurScope(), ForLoc, CoawaitLoc, FirstPart.get(),
-        ForRangeInfo.LoopVar.get(), ForRangeInfo.ColonLoc, CorrectedRange.get(),
-        T.getCloseLocation(), Sema::BFRK_Build,
+        ForRangeInfo.LoopVar.get(), ForRangeInfo.ColonLoc,
+        ForRangeInfo.RangeExpr.get(), T.getCloseLocation(), Sema::BFRK_Build,
         ForRangeInfo.LifetimeExtendTemps);
   } else if (ForEach) {
     // Similarly, we need to do the semantic analysis for a for-range
