8b0f7fc81e6312a3c4dbc3df26f505d82aa73f1d
Hans Wennborg <hans@chromium.org>
2025-06-23 13:14:09 +0200

    Revert "ELF: Add branch-to-branch optimization."
    This caused assertion failures in applyBranchToBranchOpt():
    
      llvm/include/llvm/Support/Casting.h:578:
      decltype(auto) llvm::cast(From*)
      [with To = lld::elf::InputSection; From = lld::elf::InputSectionBase]:
      Assertion `isa<To>(Val) && "cast<Ty>() argument of incompatible type!"' failed.
    
    See comment on the PR (https://github.com/llvm/llvm-project/pull/138366)
    
    This reverts commit 491b82a5ec1add78d2c93370580a2f1897b6a364.
    
    This also reverts the follow-up "[lld] Use llvm::partition_point (NFC) (#145209)"
    
    This reverts commit 2ac293f5ac4cf65c0c038bf75a88f1d6715e467d.

--- lld/ELF/Config.h
+++ lld/ELF/Config.h
@@ -304,7 +304,6 @@ struct Config {
   bool bpFunctionOrderForCompression = false;
   bool bpDataOrderForCompression = false;
   bool bpVerboseSectionOrderer = false;
-  bool branchToBranch = false;
   bool checkSections;
   bool checkDynamicRelocs;
   std::optional<llvm::DebugCompressionType> compressDebugSections;
