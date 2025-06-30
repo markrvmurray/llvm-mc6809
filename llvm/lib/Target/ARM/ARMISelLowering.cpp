c2a4ba19bc1e11099fb52e980ba2e80fee7e0110
David Green <david.green@arm.com>
2025-06-30 17:15:26 +0100

    [ARM] Add neon vector support for round
    As per #142559, this marks fround as legal for Neon and upgrades the existing
    arm.neon.vrinta intrinsics.

--- llvm/lib/Target/ARM/ARMISelLowering.cpp
+++ llvm/lib/Target/ARM/ARMISelLowering.cpp
@@ -1544,6 +1544,8 @@ ARMTargetLowering::ARMTargetLowering(const TargetMachine &TM_,
     if (Subtarget->hasV8Ops()) {
       setOperationAction(ISD::FFLOOR, MVT::v2f32, Legal);
       setOperationAction(ISD::FFLOOR, MVT::v4f32, Legal);
+      setOperationAction(ISD::FROUND, MVT::v2f32, Legal);
+      setOperationAction(ISD::FROUND, MVT::v4f32, Legal);
     }
 
     if (Subtarget->hasFullFP16()) {
@@ -1559,6 +1561,8 @@ ARMTargetLowering::ARMTargetLowering(const TargetMachine &TM_,
 
       setOperationAction(ISD::FFLOOR, MVT::v4f16, Legal);
       setOperationAction(ISD::FFLOOR, MVT::v8f16, Legal);
+      setOperationAction(ISD::FROUND, MVT::v4f16, Legal);
+      setOperationAction(ISD::FROUND, MVT::v8f16, Legal);
     }
   }
 
