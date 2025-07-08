06ec2b40556c89561f89e561c1624fc87006f28a
Jim Lin <jim@andestech.com>
2025-07-08 17:29:19 +0800

    [Triple][M68k] Add missing handling for target m68k in getDefaultExceptionHandling. (#147492)
    I encountered the assertion failure `Assertion
    TmpAsmInfo->getExceptionHandlingType() ==
    getTargetTriple().getDefaultExceptionHandling() && "MCAsmInfo and Triple
    disagree on default exception handling type"' failed`.

--- llvm/lib/TargetParser/Triple.cpp
+++ llvm/lib/TargetParser/Triple.cpp
@@ -2312,6 +2312,7 @@ ExceptionHandling Triple::getDefaultExceptionHandling() const {
   case Triple::csky:
   case Triple::hexagon:
   case Triple::lanai:
+  case Triple::m68k:
   case Triple::msp430:
   case Triple::systemz:
   case Triple::xcore:
