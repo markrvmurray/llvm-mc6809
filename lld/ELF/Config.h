9afb2a28faefdcaccd69cd3685384756316455db
Mingming Liu <mingmingl@google.com>
2025-07-23 08:29:17 -0700

    [lld][ELF] Allow `data.rel.ro.hot` and `.data.rel.ro.unlikely` to be RELRO (#148920)
    https://discourse.llvm.org/t/rfc-profile-guided-static-data-partitioning/83744
    proposes to partition a static data section (like `.data.rel.ro`) into
    two sections, one grouping the cold ones and the other grouping the
    rest.
    
    lld requires all relro sections to be contiguous. To place
    `.data.rel.ro.unlikely` in the middle of all relro sections, this change
    proposes to add `.data.rel.ro.unlikely` explicitly as RELRO section.
    
    ---------
    
    Co-authored-by: Sam Elliott <quic_aelliott@quicinc.com>

--- lld/ELF/Config.h
+++ lld/ELF/Config.h
@@ -406,6 +406,7 @@ struct Config {
   bool zIfuncNoplt;
   bool zInitfirst;
   bool zInterpose;
+  bool zKeepDataSectionPrefix;
   bool zKeepTextSectionPrefix;
   bool zLrodataAfterBss;
   bool zNoBtCfi;
