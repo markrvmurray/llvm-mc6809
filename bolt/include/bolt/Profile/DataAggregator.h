aea701ee1cd38317f9fa6d83c093154c2be20e89
Rafael Auler <rafaelauler@meta.com>
2025-08-07 15:28:22 -0700

    Revert "[BOLT][NFC] Register profiled functions once (#150622)" (#152597)
    In perf2bolt, we are observing sporadic crashes in the recently added
    registerProfiledFunctions from #150622. Addresses provided by the
    hardware (from LBR) might be -1, which clashes with what LLVM uses in
    DenseSet as empty tombstones records. This causes DenseSet to assert
    with "can't insert empty tombstone into map" when ingesting this
    data. Revert this change for now to unbreak perf2bolt.

--- bolt/include/bolt/Profile/DataAggregator.h
+++ bolt/include/bolt/Profile/DataAggregator.h
@@ -501,9 +501,6 @@ private:
   /// entries).
   void imputeFallThroughs();
 
-  /// Register profiled functions for lite mode.
-  void registerProfiledFunctions();
-
   /// Debugging dump methods
   void dump() const;
   void dump(const PerfBranchSample &Sample) const;
