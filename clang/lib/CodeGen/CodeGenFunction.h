2a0eb283d1574a379fcd1fd9326c8f208d79e297
Matheus Izvekov <mizvekov@gmail.com>
2025-08-09 05:06:53 -0300

    [clang] Improve nested name specifier AST representation (#147835)
    This is a major change on how we represent nested name qualifications in
    the AST.
    
    * The nested name specifier itself and how it's stored is changed. The
    prefixes for types are handled within the type hierarchy, which makes
    canonicalization for them super cheap, no memory allocation required.
    Also translating a type into nested name specifier form becomes a no-op.
    An identifier is stored as a DependentNameType. The nested name
    specifier gains a lightweight handle class, to be used instead of
    passing around pointers, which is similar to what is implemented for
    TemplateName. There is still one free bit available, and this handle can
    be used within a PointerUnion and PointerIntPair, which should keep
    bit-packing aficionados happy.
    * The ElaboratedType node is removed, all type nodes in which it could
    previously apply to can now store the elaborated keyword and name
    qualifier, tail allocating when present.
    * TagTypes can now point to the exact declaration found when producing
    these, as opposed to the previous situation of there only existing one
    TagType per entity. This increases the amount of type sugar retained,
    and can have several applications, for example in tracking module
    ownership, and other tools which care about source file origins, such as
    IWYU. These TagTypes are lazily allocated, in order to limit the
    increase in AST size.
    
    This patch offers a great performance benefit.
    
    It greatly improves compilation time for
    [stdexec](https://github.com/NVIDIA/stdexec). For one datapoint, for
    `test_on2.cpp` in that project, which is the slowest compiling test,
    this patch improves `-c` compilation time by about 7.2%, with the
    `-fsyntax-only` improvement being at ~12%.
    
    This has great results on compile-time-tracker as well:
    
    ![image](https://github.com/user-attachments/assets/700dce98-2cab-4aa8-97d1-b038c0bee831)
    
    This patch also further enables other optimziations in the future, and
    will reduce the performance impact of template specialization resugaring
    when that lands.
    
    It has some other miscelaneous drive-by fixes.
    
    About the review: Yes the patch is huge, sorry about that. Part of the
    reason is that I started by the nested name specifier part, before the
    ElaboratedType part, but that had a huge performance downside, as
    ElaboratedType is a big performance hog. I didn't have the steam to go
    back and change the patch after the fact.
    
    There is also a lot of internal API changes, and it made sense to remove
    ElaboratedType in one go, versus removing it from one type at a time, as
    that would present much more churn to the users. Also, the nested name
    specifier having a different API avoids missing changes related to how
    prefixes work now, which could make existing code compile but not work.
    
    How to review: The important changes are all in
    `clang/include/clang/AST` and `clang/lib/AST`, with also important
    changes in `clang/lib/Sema/TreeTransform.h`.
    
    The rest and bulk of the changes are mostly consequences of the changes
    in API.
    
    PS: TagType::getDecl is renamed to `getOriginalDecl` in this patch, just
    for easier to rebasing. I plan to rename it back after this lands.
    
    Fixes #136624
    Fixes https://github.com/llvm/llvm-project/issues/43179
    Fixes https://github.com/llvm/llvm-project/issues/68670
    Fixes https://github.com/llvm/llvm-project/issues/92757

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -2976,7 +2976,7 @@ public:
   /// member.
   bool hasVolatileMember(QualType T) {
     if (const RecordType *RT = T->getAs<RecordType>()) {
-      const RecordDecl *RD = cast<RecordDecl>(RT->getDecl());
+      const RecordDecl *RD = RT->getOriginalDecl()->getDefinitionOrSelf();
       return RD->hasVolatileMember();
     }
     return false;
@@ -4556,7 +4556,7 @@ public:
                                        ArrayRef<llvm::Value *> args);
 
   CGCallee BuildAppleKextVirtualCall(const CXXMethodDecl *MD,
-                                     NestedNameSpecifier *Qual, llvm::Type *Ty);
+                                     NestedNameSpecifier Qual, llvm::Type *Ty);
 
   CGCallee BuildAppleKextVirtualDestructorCall(const CXXDestructorDecl *DD,
                                                CXXDtorType Type,
@@ -4661,7 +4661,7 @@ public:
                                llvm::CallBase **CallOrInvoke = nullptr);
   RValue EmitCXXMemberOrOperatorMemberCallExpr(
       const CallExpr *CE, const CXXMethodDecl *MD, ReturnValueSlot ReturnValue,
-      bool HasQualifier, NestedNameSpecifier *Qualifier, bool IsArrow,
+      bool HasQualifier, NestedNameSpecifier Qualifier, bool IsArrow,
       const Expr *Base, llvm::CallBase **CallOrInvoke);
   // Compute the object pointer.
   Address EmitCXXMemberDataPointerAddress(
--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -2812,7 +2812,7 @@ void CodeGenModule::SetLLVMFunctionAttributesForDefinition(const Decl *D,
     for (const CXXRecordDecl *Base : getMostBaseClasses(MD->getParent())) {
       llvm::Metadata *Id =
           CreateMetadataIdentifierForType(Context.getMemberPointerType(
-              MD->getType(), /*Qualifier=*/nullptr, Base));
+              MD->getType(), /*Qualifier=*/std::nullopt, Base));
       F->addTypeMetadata(0, Id);
     }
   }
@@ -4171,9 +4171,11 @@ void CodeGenModule::EmitGlobal(GlobalDecl GD) {
 // Check if T is a class type with a destructor that's not dllimport.
 static bool HasNonDllImportDtor(QualType T) {
   if (const auto *RT = T->getBaseElementTypeUnsafe()->getAs<RecordType>())
-    if (CXXRecordDecl *RD = dyn_cast<CXXRecordDecl>(RT->getDecl()))
+    if (auto *RD = dyn_cast<CXXRecordDecl>(RT->getOriginalDecl())) {
+      RD = RD->getDefinitionOrSelf();
       if (RD->getDestructor() && !RD->getDestructor()->hasAttr<DLLImportAttr>())
         return true;
+    }
 
   return false;
 }
@@ -6048,7 +6050,7 @@ static bool isVarDeclStrongDefinition(const ASTContext &Context,
       return true;
 
     if (const auto *RT = VarType->getAs<RecordType>()) {
-      const RecordDecl *RD = RT->getDecl();
+      const RecordDecl *RD = RT->getOriginalDecl()->getDefinitionOrSelf();
       for (const FieldDecl *FD : RD->fields()) {
         if (FD->isBitField())
           continue;
@@ -6757,7 +6759,7 @@ QualType CodeGenModule::getObjCFastEnumerationStateType() {
     }
 
     D->completeDefinition();
-    ObjCFastEnumerationStateType = Context.getTagDeclType(D);
+    ObjCFastEnumerationStateType = Context.getCanonicalTagType(D);
   }
 
   return ObjCFastEnumerationStateType;
@@ -7267,7 +7269,8 @@ void CodeGenModule::EmitTopLevelDecl(Decl *D) {
     CXXRecordDecl *CRD = cast<CXXRecordDecl>(D);
     if (CGDebugInfo *DI = getModuleDebugInfo()) {
       if (CRD->hasDefinition())
-        DI->EmitAndRetainType(getContext().getRecordType(cast<RecordDecl>(D)));
+        DI->EmitAndRetainType(
+            getContext().getCanonicalTagType(cast<RecordDecl>(D)));
       if (auto *ES = D->getASTContext().getExternalSource())
         if (ES->hasExternalDefinitions(D) == ExternalASTSource::EK_Never)
           DI->completeUnusedClass(*CRD);
@@ -7486,20 +7489,23 @@ void CodeGenModule::EmitTopLevelDecl(Decl *D) {
   case Decl::Typedef:
   case Decl::TypeAlias: // using foo = bar; [C++11]
     if (CGDebugInfo *DI = getModuleDebugInfo())
-      DI->EmitAndRetainType(
-          getContext().getTypedefType(cast<TypedefNameDecl>(D)));
+      DI->EmitAndRetainType(getContext().getTypedefType(
+          ElaboratedTypeKeyword::None, /*Qualifier=*/std::nullopt,
+          cast<TypedefNameDecl>(D)));
     break;
 
   case Decl::Record:
     if (CGDebugInfo *DI = getModuleDebugInfo())
       if (cast<RecordDecl>(D)->getDefinition())
-        DI->EmitAndRetainType(getContext().getRecordType(cast<RecordDecl>(D)));
+        DI->EmitAndRetainType(
+            getContext().getCanonicalTagType(cast<RecordDecl>(D)));
     break;
 
   case Decl::Enum:
     if (CGDebugInfo *DI = getModuleDebugInfo())
       if (cast<EnumDecl>(D)->getDefinition())
-        DI->EmitAndRetainType(getContext().getEnumType(cast<EnumDecl>(D)));
+        DI->EmitAndRetainType(
+            getContext().getCanonicalTagType(cast<EnumDecl>(D)));
     break;
 
   case Decl::HLSLBuffer:
@@ -7953,8 +7959,8 @@ bool CodeGenModule::NeedAllVtablesTypeId() const {
 void CodeGenModule::AddVTableTypeMetadata(llvm::GlobalVariable *VTable,
                                           CharUnits Offset,
                                           const CXXRecordDecl *RD) {
-  llvm::Metadata *MD =
-      CreateMetadataIdentifierForType(QualType(RD->getTypeForDecl(), 0));
+  CanQualType T = getContext().getCanonicalTagType(RD);
+  llvm::Metadata *MD = CreateMetadataIdentifierForType(T);
   VTable->addTypeMetadata(Offset.getQuantity(), MD);
 
   if (CodeGenOpts.SanitizeCfiCrossDso)
