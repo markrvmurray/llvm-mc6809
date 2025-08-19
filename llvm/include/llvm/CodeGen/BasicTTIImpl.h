576a8b0f44de211754cd3b57e4f48ce5f533d3a3
David Sherwood <david.sherwood@arm.com>
2025-08-19 09:31:37 +0100

    [LV][TTI] Calculate cost of extracting last index in a scalable vector (#144086)
    There are a couple of places in the loop vectoriser where we
    want to calculate the cost of extracting the last lane in a
    vector. However, we wrongly assume that asking for the cost
    of extracting lane (VF.getKnownMinValue() - 1) is an accurate
    representation of the cost of extracting the last lane. For
    SVE at least, this is non-trivial as it requires the use of
    whilelo and lastb instructions.
    
    To solve this problem I have added a new
    getReverseVectorInstrCost interface where the index is used
    in reverse from the end of the vector. Suppose a vector has
    a given ElementCount EC, the extracted/inserted lane would be
    EC - 1 - Index. For scalable vectors this index is unknown at
    compile time. I've added a AArch64 hook that better represents
    the cost, and also a RISCV hook that maintains compatibility
    with the behaviour prior to this PR.
    
    I've also taken the liberty of adding support in vplan for
    calculating the cost of VPInstruction::ExtractLastElement.

--- llvm/include/llvm/CodeGen/BasicTTIImpl.h
+++ llvm/include/llvm/CodeGen/BasicTTIImpl.h
@@ -1461,6 +1461,20 @@ public:
   }
 
   InstructionCost
+  getIndexedVectorInstrCostFromEnd(unsigned Opcode, Type *Val,
+                                   TTI::TargetCostKind CostKind,
+                                   unsigned Index) const override {
+    unsigned NewIndex = -1;
+    if (auto *FVTy = dyn_cast<FixedVectorType>(Val)) {
+      assert(Index < FVTy->getNumElements() &&
+             "Unexpected index from end of vector");
+      NewIndex = FVTy->getNumElements() - 1 - Index;
+    }
+    return thisT()->getVectorInstrCost(Opcode, Val, CostKind, NewIndex, nullptr,
+                                       nullptr);
+  }
+
+  InstructionCost
   getReplicationShuffleCost(Type *EltTy, int ReplicationFactor, int VF,
                             const APInt &DemandedDstElts,
                             TTI::TargetCostKind CostKind) const override {
