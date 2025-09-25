5b35b2fbe5f8e6d1c876c9d830fe507dd2712200
Philip Reames <preames@rivosinc.com>
2025-09-24 18:52:17 -0700

    [TII] Split isTrivialReMaterializable into two versions [nfc] (#160377)
    This change builds on https://github.com/llvm/llvm-project/pull/160319
    which tries to clarify which *callers* (not backends) assume that the
    result is actually trivial.
    
    This change itself should be NFC. Essentially, I'm just renaming the
    existing isTrivialRematerializable to the non-trivial version and then
    adding a new trivial version (with the same name as the prior function)
    and simplifying a few callers which want that semantic.
    
    This change does *not* enable non-trivial remat any more broadly than
    was already done for our targets which were lying through the old APIs;
    that will come separately. The goal here is simply to make the code
    easier to follow in terms of what assumptions are being made where.
    
    ---------
    
    Co-authored-by: Luke Lau <luke_lau@icloud.com>

--- llvm/lib/CodeGen/LiveRangeEdit.cpp
+++ llvm/lib/CodeGen/LiveRangeEdit.cpp
@@ -92,7 +92,7 @@ void LiveRangeEdit::scanRemattable() {
     MachineInstr *DefMI = LIS.getInstructionFromIndex(OrigVNI->def);
     if (!DefMI)
       continue;
-    if (TII.isTriviallyReMaterializable(*DefMI))
+    if (TII.isReMaterializable(*DefMI))
       Remattable.insert(OrigVNI);
   }
   ScannedRemattable = true;
@@ -403,7 +403,7 @@ void LiveRangeEdit::eliminateDeadDef(MachineInstr *MI, ToShrinkSet &ToShrink) {
     // register uses. That may provoke RA to split an interval at the KILL
     // and later result in an invalid live segment end.
     if (isOrigDef && DeadRemats && !HasLiveVRegUses &&
-        TII.isTriviallyReMaterializable(*MI)) {
+        TII.isReMaterializable(*MI)) {
       LiveInterval &NewLI = createEmptyIntervalFrom(Dest, false);
       VNInfo::Allocator &Alloc = LIS.getVNInfoAllocator();
       VNInfo *VNI = NewLI.getNextValue(Idx, Alloc);
