markrvmurray/llvm-mc6809

commit 8bbde2453d05ee2e7cfa0e2bcdb7f9860274f963

[X86][APX] Fold NDD+Load to non-NDD when NDD memory variant is not preferred (#189222)

llvm/lib/Target/X86/X86InstrInfo.cpp
@@ -7539,13 +7539,12 @@ MachineInstr *X86InstrInfo::foldMemoryOperandImpl(
   //
   // Utilize the mapping NonNDD -> RMW for the NDD variant.
   unsigned NonNDOpc = Subtarget.hasNDD() ? X86::getNonNDVariant(Opc) : 0U;
-  // Disable memory folding for NDD instructions.
-  if (NonNDOpc && !Subtarget.hasNDDM())
-    return nullptr;
+  // Utilize the mapping NonNDD if NDD memory variant is not preferred.
+  bool NoNDDM = NonNDOpc && !Subtarget.hasNDDM();
 
   const X86FoldTableEntry *I =
       IsTwoAddr ? lookupTwoAddrFoldTable(NonNDOpc ? NonNDOpc : Opc)
-                : lookupFoldTable(Opc, OpNum);
+                : lookupFoldTable(NoNDDM ? NonNDOpc : Opc, OpNum);
 
   MachineInstr *NewMI = nullptr;
   if (I) {
@@ -7591,6 +7590,20 @@ MachineInstr *X86InstrInfo::foldMemoryOperandImpl(
         NewMI->getOperand(0).setReg(RI.getSubReg(DstReg, X86::sub_32bit));
       else
         NewMI->getOperand(0).setSubReg(X86::sub_32bit);
+    }
+
+    if (NoNDDM) {
+      Register SrcReg = MI.getOperand(1).getReg();
+      if (MI.killsRegister(SrcReg, /*TRI=*/nullptr))
+        return NewMI;
+
+      const TargetRegisterClass &RC = *MF.getRegInfo().getRegClass(SrcReg);
+      Register NewSrc = MF.getRegInfo().createVirtualRegister(&RC);
+      BuildMI(*NewMI->getParent(), *NewMI, MI.getDebugLoc(),
+              get(TargetOpcode::COPY))
+          .addReg(NewSrc, RegState::Define)
+          .addReg(SrcReg);
+      NewMI->getOperand(1).setReg(NewSrc);
     }
     return NewMI;
   }

llvm/test/CodeGen/X86/apx/adc.ll
@@ -59,9 +59,10 @@ define i64 @adc64rr(i64 %a, i64 %b, i64 %x, i64 %y) nounwind {
 define i8 @adc8rm(i8 %a, ptr %ptr, i8 %x, i8 %y) nounwind {
 ; NDD-LABEL: adc8rm:
 ; NDD:       # %bb.0:
-; NDD-NEXT:    movzbl (%rsi), %eax # encoding: [0x0f,0xb6,0x06]
+; NDD-NEXT:    movl %edi, %eax # encoding: [0x89,0xf8]
 ; NDD-NEXT:    cmpb %dl, %cl # encoding: [0x38,0xd1]
-; NDD-NEXT:    adcb %dil, %al # EVEX TO LEGACY Compression encoding: [0x40,0x10,0xf8]
+; NDD-NEXT:    adcb (%rsi), %al # encoding: [0x12,0x06]
+; NDD-NEXT:    # kill: def $al killed $al killed $eax
 ; NDD-NEXT:    retq # encoding: [0xc3]
 ;
 ; MEM-LABEL: adc8rm:
@@ -80,9 +81,10 @@ define i8 @adc8rm(i8 %a, ptr %ptr, i8 %x, i8 %y) nounwind {
 define i16 @adc16rm(i16 %a, ptr %ptr, i16 %x, i16 %y) nounwind {
 ; NDD-LABEL: adc16rm:
 ; NDD:       # %bb.0:
-; NDD-NEXT:    movzwl (%rsi), %eax # encoding: [0x0f,0xb7,0x06]
+; NDD-NEXT:    movl %edi, %eax # encoding: [0x89,0xf8]
 ; NDD-NEXT:    cmpw %dx, %cx # encoding: [0x66,0x39,0xd1]
-; NDD-NEXT:    adcw %di, %ax # EVEX TO LEGACY Compression encoding: [0x66,0x11,0xf8]
+; NDD-NEXT:    adcw (%rsi), %ax # encoding: [0x66,0x13,0x06]
+; NDD-NEXT:    # kill: def $ax killed $ax killed $eax
 ; NDD-NEXT:    retq # encoding: [0xc3]
 ;
 ; MEM-LABEL: adc16rm:
@@ -101,9 +103,9 @@ define i16 @adc16rm(i16 %a, ptr %ptr, i16 %x, i16 %y) nounwind {
 define i32 @adc32rm(i32 %a, ptr %ptr, i32 %x, i32 %y) nounwind {
 ; NDD-LABEL: adc32rm:
 ; NDD:       # %bb.0:
-; NDD-NEXT:    movl (%rsi), %eax # encoding: [0x8b,0x06]
+; NDD-NEXT:    movl %edi, %eax # encoding: [0x89,0xf8]
 ; NDD-NEXT:    cmpl %edx, %ecx # encoding: [0x39,0xd1]
-; NDD-NEXT:    adcl %edi, %eax # EVEX TO LEGACY Compression encoding: [0x11,0xf8]
+; NDD-NEXT:    adcl (%rsi), %eax # encoding: [0x13,0x06]
 ; NDD-NEXT:    retq # encoding: [0xc3]
 ;
 ; MEM-LABEL: adc32rm:
@@ -122,9 +124,9 @@ define i32 @adc32rm(i32 %a, ptr %ptr, i32 %x, i32 %y) nounwind {
 define i64 @adc64rm(i64 %a, ptr %ptr, i64 %x, i64 %y) nounwind {
 ; NDD-LABEL: adc64rm:
 ; NDD:       # %bb.0:
-; NDD-NEXT:    movq (%rsi), %rax # encoding: [0x48,0x8b,0x06]
+; NDD-NEXT:    movq %rdi, %rax # encoding: [0x48,0x89,0xf8]
 ; NDD-NEXT:    cmpq %rdx, %rcx # encoding: [0x48,0x39,0xd1]
-; NDD-NEXT:    adcq %rdi, %rax # EVEX TO LEGACY Compression encoding: [0x48,0x11,0xf8]
+; NDD-NEXT:    adcq (%rsi), %rax # encoding: [0x48,0x13,0x06]
 ; NDD-NEXT:    retq # encoding: [0xc3]
 ;
 ; MEM-LABEL: adc64rm:
@@ -237,9 +239,10 @@ define i64 @adc64ri(i64 %a, i64 %x, i64 %y) nounwind {
 define i8 @adc8mr(i8 %a, ptr %ptr, i8 %x, i8 %y) nounwind {
 ; NDD-LABEL: adc8mr:
 ; NDD:       # %bb.0:
-; NDD-NEXT:    movzbl (%rsi), %eax # encoding: [0x0f,0xb6,0x06]
+; NDD-NEXT:    movl %edi, %eax # encoding: [0x89,0xf8]
 ; NDD-NEXT:    cmpb %dl, %cl # encoding: [0x38,0xd1]
-; NDD-NEXT:    adcb %dil, %al # EVEX TO LEGACY Compression encoding: [0x40,0x10,0xf8]
+; NDD-NEXT:    adcb (%rsi), %al # encoding: [0x12,0x06]
+; NDD-NEXT:    # kill: def $al killed $al killed $eax
 ; NDD-NEXT:    retq # encoding: [0xc3]
 ;
 ; MEM-LABEL: adc8mr:
@@ -258,9 +261,10 @@ define i8 @adc8mr(i8 %a, ptr %ptr, i8 %x, i8 %y) nounwind {
 define i16 @adc16mr(i16 %a, ptr %ptr, i16 %x, i16 %y) nounwind {
 ; NDD-LABEL: adc16mr:
 ; NDD:       # %bb.0:
-; NDD-NEXT:    movzwl (%rsi), %eax # encoding: [0x0f,0xb7,0x06]
+; NDD-NEXT:    movl %edi, %eax # encoding: [0x89,0xf8]
 ; NDD-NEXT:    cmpw %dx, %cx # encoding: [0x66,0x39,0xd1]
-; NDD-NEXT:    adcw %di, %ax # EVEX TO LEGACY Compression encoding: [0x66,0x11,0xf8]
+; NDD-NEXT:    adcw (%rsi), %ax # encoding: [0x66,0x13,0x06]
+; NDD-NEXT:    # kill: def $ax killed $ax killed $eax
 ; NDD-NEXT:    retq # encoding: [0xc3]
 ;
 ; MEM-LABEL: adc16mr:
@@ -279,9 +283,9 @@ define i16 @adc16mr(i16 %a, ptr %ptr, i16 %x, i16 %y) nounwind {
 define i32 @adc32mr(i32 %a, ptr %ptr, i32 %x, i32 %y) nounwind {
 ; NDD-LABEL: adc32mr:
 ; NDD:       # %bb.0:
-; NDD-NEXT:    movl (%rsi), %eax # encoding: [0x8b,0x06]
+; NDD-NEXT:    movl %edi, %eax # encoding: [0x89,0xf8]
 ; NDD-NEXT:    cmpl %edx, %ecx # encoding: [0x39,0xd1]
-; NDD-NEXT:    adcl %edi, %eax # EVEX TO LEGACY Compression encoding: [0x11,0xf8]
+; NDD-NEXT:    adcl (%rsi), %eax # encoding: [0x13,0x06]
 ; NDD-NEXT:    retq # encoding: [0xc3]
 ;
 ; MEM-LABEL: adc32mr:
@@ -300,9 +304,9 @@ define i32 @adc32mr(i32 %a, ptr %ptr, i32 %x, i32 %y) nounwind {
 define i64 @adc64mr(i64 %a, ptr %ptr, i64 %x, i64 %y) nounwind {
 ; NDD-LABEL: adc64mr:
 ; NDD:       # %bb.0:
-; NDD-NEXT:    movq (%rsi), %rax # encoding: [0x48,0x8b,0x06]
+; NDD-NEXT:    movq %rdi, %rax # encoding: [0x48,0x89,0xf8]
 ; NDD-NEXT:    cmpq %rdx, %rcx # encoding: [0x48,0x39,0xd1]
-; NDD-NEXT:    adcq %rdi, %rax # EVEX TO LEGACY Compression encoding: [0x48,0x11,0xf8]
+; NDD-NEXT:    adcq (%rsi), %rax # encoding: [0x48,0x13,0x06]
 ; NDD-NEXT:    retq # encoding: [0xc3]
 ;
 ; MEM-LABEL: adc64mr:

commit 35025e2b5da1b6081298b112bd438d7c2c6781ff

[X86][APX] Fix segfault in foldMemoryOperandImpl for two-address NDD fold (#190562)
The NoNDDM code path in foldMemoryOperandImpl assumed
NewMI->getOperand(1) is always a register. When IsTwoAddr is true,
fuseTwoAddrInst replaces operands 0-4 with memory address components, so
getOperand(1) is the immediate, not a register. Calling setReg() causes
a segfault in removeOperandFromUseList.

Skip the NoNDDM COPY block when IsTwoAddr is true, since the two-address
fold already correctly handles the dest==src1 constraint.

I believe the issue was introduced with #189222 , the 'NoNDDM' block
calls 'NewMI->getOperand(1).setReg()', but after 'fuseTwoAddrInst',
operand 1 is an immediate, not a register.

Passes all APX regression tests. Unit test included in commit. Fixes
issue #190557.

First time submitting a PR to the LLVM project, please let me know if I
need to fix something! Tagging @phoebewang and @RKSimon as potential
review candidates.

llvm/lib/Target/X86/X86InstrInfo.cpp
@@ -7592,7 +7592,7 @@ MachineInstr *X86InstrInfo::foldMemoryOperandImpl(
         NewMI->getOperand(0).setSubReg(X86::sub_32bit);
     }
 
-    if (NoNDDM) {
+    if (NoNDDM && !IsTwoAddr) {
       Register SrcReg = MI.getOperand(1).getReg();
       if (MI.killsRegister(SrcReg, /*TRI=*/nullptr))
         return NewMI;

llvm/test/CodeGen/X86/apx/pr190557.ll
@@ -1,0 +1,58 @@
+; NOTE: Assertions have been autogenerated by utils/update_llc_test_checks.py
+; RUN: llc < %s -mtriple=x86_64-unknown -mattr=+ndd -verify-machineinstrs | FileCheck %s
+;
+; Verify that folding NDD instructions to non-NDD with memory operands does not
+; crash when the fold goes through the two-address path (IsTwoAddr=true).
+; This is a regression test for a segfault in foldMemoryOperandImpl where
+; NewMI->getOperand(1).setReg() was called on a memory operand instead of a
+; register operand after fuseTwoAddrInst.
+
+define void @ndd_fold_twoaddr(ptr %0, i64 %1, ptr %2, i64 %3, i1 %4) nounwind {
+; CHECK-LABEL: ndd_fold_twoaddr:
+; CHECK:       # %bb.0:
+; CHECK-NEXT:    pushq %rbp
+; CHECK-NEXT:    pushq %r15
+; CHECK-NEXT:    pushq %r14
+; CHECK-NEXT:    pushq %r13
+; CHECK-NEXT:    pushq %r12
+; CHECK-NEXT:    pushq %rbx
+; CHECK-NEXT:    subq $24, %rsp
+; CHECK-NEXT:    movl %r8d, %ebx
+; CHECK-NEXT:    movq %rcx, %r14
+; CHECK-NEXT:    movq %rdx, %r15
+; CHECK-NEXT:    movq %rsi, {{[-0-9]+}}(%r{{[sb]}}p) # 8-byte Spill
+; CHECK-NEXT:    movq %rdi, %r13
+; CHECK-NEXT:    movq $0, {{[-0-9]+}}(%r{{[sb]}}p) # 8-byte Folded Spill
+; CHECK-NEXT:    movq %rcx, %r12
+; CHECK-NEXT:    movq %rcx, %rbp
+; CHECK-NEXT:    .p2align 4
+; CHECK-NEXT:  .LBB0_2: # =>This Inner Loop Header: Depth=1
+; CHECK-NEXT:    movq %rbp, %rdi
+; CHECK-NEXT:    movq %r13, %rsi
+; CHECK-NEXT:    callq *%r15
+; CHECK-NEXT:    addq %r14, %rbp
+; CHECK-NEXT:    testb $1, %bl
+; CHECK-NEXT:    jne .LBB0_2
+; CHECK-NEXT:  # %bb.1: # in Loop: Header=BB0_2 Depth=1
+; CHECK-NEXT:    movq {{[-0-9]+}}(%r{{[sb]}}p), %rax # 8-byte Reload
+; CHECK-NEXT:    addq %rax, {{[-0-9]+}}(%r{{[sb]}}p) # 8-byte Folded Reload
+; CHECK-NEXT:    addq %rax, %r12
+; CHECK-NEXT:    movq %r12, %rbp
+; CHECK-NEXT:    jmp .LBB0_2
+  br label %6
+
+6:
+  %7 = phi ptr [ %10, %9 ], [ null, %5 ]
+  %8 = icmp ugt ptr %7, null
+  br label %11
+
+9:
+  %10 = getelementptr i8, ptr %7, i64 %1
+  br label %6
+
+11:
+  %12 = phi ptr [ %13, %11 ], [ %7, %6 ]
+  %13 = getelementptr i8, ptr %12, i64 %3
+  %14 = tail call i32 %2(ptr %13, ptr %0)
+  br i1 %4, label %11, label %9
+}